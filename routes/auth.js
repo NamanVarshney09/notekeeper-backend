2a110334130924dcf9a6342535ff22ce097070d5
Naman Varshney <namanvarshney09@gmail.com>
2023-03-07 19:36:15 +0530

    Created Login endpoint and sending authToken

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // express-validator is a set of express.js middlewares that wraps validator.js validator and sanitizer functions.
 const { body, validationResult } = require('express-validator');
 
+const JWT_SECRET = "TheDarkLord";
+
 /* 
-    Create a User using POST "/api/auth/createuser"
+    TODO: Create a User using POST "/api/auth/createuser"
     *No Login required
 */
 router.post('/createuser', [
@@ -14,7 +18,6 @@ router.post('/createuser', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be at least 8 characters long').isLength({ min: 8 }),
 ], async (req, res) => {
-
     //Returns bad request and errors if any validation fails
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -26,19 +29,64 @@ router.post('/createuser', [
             ? find().limit(1) is significantly faster than findOne
             find().limit(1) returns a cursor while findOne() will read the document, return it to you, and close the cursor if the record exists
         */
-        let user = await User.find({ email: req.body.email }).limit(1);
+        let user = await User.findOne({ email: req.body.email });
         if (user) {
             return res.status(400).json({ error: "This email is already registered" })
         }
+
+        const salt = await bcrypt.genSalt(10);
+        const securePassword = await bcrypt.hash(req.body.password, salt);
+
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
-        })
-        res.json(user)
+            password: securePassword
+        });
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({ authToken })
     } catch (error) {
         console.error(error.message);
-        res.status(500).json("Internal error occurred !")
+        res.status(500).json("Internal error occurred !");
+    }
+})
+
+
+/* 
+    TODO: Create a User using POST "/api/auth/login"
+    *No Login required
+*/
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists()
+], async (req, res) => {
+    //Returns bad request and errors if any validation fails
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user)
+            return res.status(400).json({ error: "Please enter correct credentials" });
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch)
+            return res.status(400).json({ error: "Please enter correct credentials" });
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({ authToken })
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json("Internal server error occurred !");
     }
 })
 
